367f713a613aca8e907b38dbb896b1ed4d404d3c
Upatel98 <usamahpatel21@gmail.com>
2022-11-30 23:22:00 +0000

    Week 6 - 3.0

--- mini_project.py
+++ mini_project.py
@@ -5,9 +5,7 @@ from mp_ecosystem import products, couriers, orders
 
 while True:
   print('\n---Welcome---\n[Index 0] ==> Exit App\n[Index 1] ==> Products Menu\n[Index 2] ==> Couriers Menu\n[Index 3] ==> Orders Menu')
-
-  entr = input('Enter Index: ')
-  entr = inpt(entr, 4)
+  entr = inpt(input('Enter Index: '), 4)
   clr_trmnl()
 
   if entr == 0:
@@ -19,8 +17,7 @@ while True:
   elif entr == 1: 
     while True:
       print('\n---Product Menu---\n[Index 0] ==> Return to Main Menu\n[Index 1] ==> Products Menu\n[Index 2] ==> Add New Item\n[Index 3] ==> Update Item\n[Index 4] ==> Delete an Item')
-      entr = input('Enter Index: ')
-      entr = inpt(entr, 6)
+      entr = inpt(input('Enter Index: '), 6)
 
       if entr == 0:
         clr_trmnl()
@@ -42,8 +39,7 @@ while True:
   elif entr == 2: 
     while True: 
       print('\n---Courier Menu---\n[Index 0] ==> Return to Main Menu\n[Index 1] ==> Couriers\n[Index 2] ==> Add Courier\n[Index 3] ==> Update Courier\n[Index 4] ==> Delete Courier')
-      entr = input('Enter Index: ')
-      entr = inpt(entr, 5)
+      entr = inpt(input('Enter Index: '), 5)
     
       if entr == 0:
         clr_trmnl()
@@ -65,8 +61,7 @@ while True:
   elif entr == 3:
     while True: 
       print('\n---Orders Menu---\n[Index 0] ==> Return to Main Menu\n[Index 1] ==> Orders Directory\n[Index 2] ==> Add Customer Information\n[Index 3] ==> Update Order Status\n[Index 4] ==> Update Customer Information\n[Index 5] ==> Delete Customer Information')
-      entr = input('Enter Index: ')
-      entr = inpt(entr, 6)
+      entr = inpt(input('Enter Index: '), 6)
 
       if entr == 0:
         clr_trmnl()
